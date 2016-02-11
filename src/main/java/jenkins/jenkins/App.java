9b138238f6419cae64ee3d95478990eba942382a
swendleys <sprott95@gmail.com>
2016-02-11 13:40:26 +0100

    'ss'

---
+++ src/main/java/jenkins/jenkins/App.java
@@ -0,0 +1,13 @@
+package jenkins.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
